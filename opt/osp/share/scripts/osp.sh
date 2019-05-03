d5e255b4f92dde8de42c6429deaa1f6b2915d635
Sean Hamilton <skhamilt@eng.ucsd.edu>
2019-05-02 20:30:16 -0700

    updated for v7

--- opt/osp/share/scripts/osp.sh
+++ opt/osp/share/scripts/osp.sh
@@ -75,11 +75,19 @@ buildKernel() {
   printf "  Installing modules....................... "
   make modules_install KERNELRELEASE=4.14.0-qcomlt-arm64 INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=$ROOTFS_DIR V=$KERNEL_MAKE_V &>> $REDIRECT
   printf "DONE\n"
-  printf "  Building boot image...................... "
+  printf "  Building boot images..................... "
   cat arch/arm64/boot/Image.gz arch/arm64/boot/dts/qcom/apq8016-sbc.dtb > $OSP_BUILD/Image.gz+dtb
   mkbootimg --kernel $OSP_BUILD/Image.gz+dtb \
             --ramdisk $OSP_ROOT/share/initrd.img \
             --output $OSP_BUILD/boot.img \
+            --pagesize 2048 \
+            --base 0x80000000 \
+            --cmdline "root=/dev/disk/by-partlabel/rootfs rw rootwait console=ttyMSM0,115200n8"
+  rm -f $OSP_BUILD/Image.gz+dtb &>> /dev/null
+  cat arch/arm64/boot/Image.gz arch/arm64/boot/dts/qcom/ospboardv7.dtb > $OSP_BUILD/Image.gz+dtb
+  mkbootimg --kernel $OSP_BUILD/Image.gz+dtb \
+            --ramdisk $OSP_ROOT/share/initrd.img \
+            --output $OSP_BUILD/boot-v7.img \
             --pagesize 2048 \
             --base 0x80000000 \
             --cmdline "root=/dev/disk/by-partlabel/rootfs rw rootwait console=ttyMSM0,115200n8"
@@ -91,27 +99,6 @@ buildKernel() {
   if [ -z "$1" ] || [ "$1" != "skip" ]; then
     updateRootfs "  "
   fi
-}
-
-buildOSP() {
-  echo "OSP Build Tasks:"
-  pushd $OSP_BUILD &>> /dev/null
-  cmake -DCMAKE_TOOLCHAIN_FILE=/opt/osp/src/CMakeToolchain /opt/osp/src/osp-process
-  make
-  cp osp_clion /opt/osp/rootfs/usr/local/bin/osp_process
-  popd &>> /dev/null
-
-  if [ -z "$1" ] || [ "$1" != "skip" ]; then
-    updateRootfs "  "
-  fi
-}
-
-buildAll() {
-  buildLK
-  buildKernel skip
-  buildOSP skip
-  updateRootfs
-  compressRootfs
 }
 
 buildLK() {
@@ -137,8 +124,6 @@ buildHelp() {
     echo '  compressRootfs -- updates rootfs.simg.gz with latest rootfs.simg'
     echo '  buildKernel    -- builds the kernel and creates image'
     echo '  buildLK        -- builds the Little Kernel bootloader image'
-    echo '  buildOSP       -- builds OSP process and installs in rootfs image'
-    echo '  buildAll       -- builds all and updates rootfs image'
     echo '  setOutput      -- sets the output to "quiet, verbose (default), or log"'
     echo '  buildHelp      -- print this help summary'
 }
